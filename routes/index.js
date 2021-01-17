4638208eb854c659551673102fe94ad879a0cae9
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-17 02:55:13 -0600

    adding modules

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var mysql = require("mysql");
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
+    conn = mysql.createConnection({
         host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         database: "sosbxf9opvllpwal",
         user: "u1emj7frzaensog3",
@@ -28,7 +28,7 @@ conn.connect(function(error){
     }
 })
 
-var PORT = process.env.PORT || 3000;
+//var PORT = process.env.PORT || 3000;
 
 
 router.get('/', (req, res)=>{
@@ -253,4 +253,4 @@ router.post('/modify/:id', (req, res)=>{
 
 
 module.exports = router;
-module.exports = connection;
+
