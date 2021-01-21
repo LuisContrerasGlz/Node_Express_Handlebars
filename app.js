d1cdc8d9ba0d0ca301aa8732f8dd29cb494c0cb0
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-01-20 20:40:14 -0600

    change connection port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const morgan = require('morgan');
 
 // settings
-app.set('port',3000);
+//app.set('port',3000);
 var PORT = process.env.PORT || 3000;
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
